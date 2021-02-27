d3d512ab6c4813add39747c92dbe2b29bc6ceae3
VictorTomasCarretero <vicato99@gmail.com>
2021-02-27 01:55:11 +0100

    Archivos Inicales

---
+++ vendor/database/config.php
@@ -0,0 +1,20 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+ini_set("display_errors", true);
+
+$host = "localhost";
+$port = 3306;
+$db_username = "root";
+$db_password = "";
+$db_name = "harrypotter";
+
+require_once("Database.php");
+
+$db=new Database($host, $port, $db_username, $db_password, $db_name);
+
+function handleException($exception)
+{
+    echo  $exception->getMessage();
+}
+
+set_exception_handler('handleException');
